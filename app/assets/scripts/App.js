230cd51b609abb65f45497361e5f2a86bc8bf12e
Caleb Bouma <caleb.bouma@economicmodeling.com>
2020-11-15 21:27:09 -0800

    'Created an App.js file and configured webpack to update it.'

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,1 @@
+alert("Hello. This is just a test to pull in script into a main file using webpack! And now we're proving that webpack is watching for files continuously! :)");
---
+++ app/bundled.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!***********************************!*\
+  !*** ./app/assets/scripts/App.js ***!
+  \***********************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("alert(\"Hello. This is just a test to pull in script into a main file using webpack! And now we're proving that webpack is watching for files continuously! :)\");\r\n\n\n//# sourceURL=webpack://practice-sites-travel/./app/assets/scripts/App.js?");
+/******/ })()
+;
