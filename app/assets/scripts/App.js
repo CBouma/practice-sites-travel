8038717e1c801fd4d3e16ecc4f5768edec0a8f00
Caleb Bouma <caleb.bouma@economicmodeling.com>
2020-11-18 20:55:01 -0800

    add styles.css file

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,2 @@
-alert("Hello. This is just a test to pull in script into a main file using webpack! And now we're proving that webpack is watching for files continuously! :)");
+import  '../styles/styles.css';
+// alert("javascript is fun!");
